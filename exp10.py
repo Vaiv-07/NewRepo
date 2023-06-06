a49e4e67a3e6701a851e4da783102f4661a6e682
Vaiv-07 <105795168+Vaiv-07@users.noreply.github.com>
2023-06-06 09:47:44 +0530

    Add files via upload

---
+++ exp10.py
@@ -0,0 +1,82 @@
+# Merge Sort
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left = arr[:mid]
+    right = arr[mid:]
+
+    left = merge_sort(left)
+    right = merge_sort(right)
+
+    return merge(left, right)
+
+
+def merge(left, right):
+    merged = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            merged.append(left[i])
+            i += 1
+        else:
+            merged.append(right[j])
+            j += 1
+
+    while i < len(left):
+        merged.append(left[i])
+        i += 1
+
+    while j < len(right):
+        merged.append(right[j])
+        j += 1
+
+    return merged
+
+
+# Selection Sort
+def selection_sort(arr):
+    for i in range(len(arr)):
+        min_index = i
+
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[min_index]:
+                min_index = j
+
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+
+    return arr
+
+
+# Insertion Sort
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+
+        arr[j + 1] = key
+
+    return arr
+
+
+# Example usage
+arr = [8, 5, 2, 9, 5, 6, 3]
+print("Original array:", arr)
+
+# Merge Sort
+sorted_arr = merge_sort(arr)
+print("Merge Sort:", sorted_arr)
+
+# Selection Sort
+sorted_arr = selection_sort(arr)
+print("Selection Sort:", sorted_arr)
+
+# Insertion Sort
+sorted_arr = insertion_sort(arr)
+print("Insertion Sort:", sorted_arr)
